ee9cc0cfed674a5701816fe23254d747eff51e65
fukuda <kfukuda@student.42tokyo.jp>
2022-02-25 09:32:17 +0900

    [add]Goodコード完成

---
+++ OCP/Good/Archer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCP.Good
+{
+    // 弓使い
+    class Archer : CharacterBase
+    {
+        protected override int GetSwordAttackDamageSub()
+        {
+            return Attack;
+        }
+
+        protected override int GetBowAttackDamageSub()
+        {
+            return GetPowerUpDamage();
+        }
+
+        protected override int GetMagicAttackDamageSub()
+        {
+            return GetPowerDownDamage();
+        }
+
+    }
+}
---
+++ OCP/Good/Wizard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCP.Good
+{
+    // 魔法使い
+    class Wizard : CharacterBase
+    {
+        protected override int GetSwordAttackDamageSub()
+        {
+            return GetPowerDownDamage();
+        }
+
+        protected override int GetBowAttackDamageSub()
+        {
+            return Attack;
+        }
+
+        protected override int GetMagicAttackDamageSub()
+        {
+            return GetPowerUpDamage();
+        }
+
+    }
+}
