876a7fd1cdffdd244015eaa820e9f5d6534b8c50
fukuda <kfukuda@student.42tokyo.jp>
2022-02-28 19:58:38 +0900

    add IPoint
    ポイントインターフェース

---
+++ OCP/GoodInterface/IPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCP.GoodInterface
+{
+    interface IPoint
+    {
+        protected enum memberType
+        {
+            NORMAL,
+            SILVER,
+            GOLD
+        };
+
+        public int GetPoint();
+    }
+}
