9a936f889f383165752b9d90c6c6c3f29b784562
fukuda <kfukuda@student.42tokyo.jp>
2022-02-28 20:17:48 +0900

    add Factories

---
+++ OCP/Factories/Factories.cs
@@ -0,0 +1,26 @@
+﻿using OCP.GoodInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCP.Factories
+{
+    class Factories
+    {
+        public static IPoint CreatePoint(string memberNo)
+        {
+            if (memberNo.StartsWith("G"))
+            {
+                return new GoldMemberPoint();
+            }
+
+            if (memberNo.StartsWith("S"))
+            {
+                return new SilverMemberPoint();
+            }
+
+            return new NormalMemberPoint();
+
+        }
+    }
+}
