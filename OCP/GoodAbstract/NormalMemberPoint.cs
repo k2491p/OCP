9c8a922f9abc532dc9d0c1429fbef61c5cded798
fukuda <kfukuda@student.42tokyo.jp>
2022-03-01 20:08:13 +0900

    add GoodAbstract:Normal, Siliver, Gold

---
+++ OCP/GoodAbstract/NormalMemberPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCP.GoodAbstract
+{
+    sealed class NormalMemberPoint : PointBase
+    {
+        protected override int GetPointSub(int price)
+        {
+            return (int)(price * 0.01);
+        }
+    }
+}
