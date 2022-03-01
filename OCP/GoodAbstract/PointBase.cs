1f7dffadd7be82569b92d6a501202d6eec00e52f
fukuda <kfukuda@student.42tokyo.jp>
2022-03-01 20:07:51 +0900

    add PointBase

---
+++ OCP/GoodAbstract/PointBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCP.GoodAbstract
+{
+    abstract class PointBase
+    {
+        public int GetPoint(int price)
+        {
+            var point = GetPointSub(price);
+            if (DateTime.Now.Day == 1)
+            {
+                point *= 10;
+            }
+            return point;
+        }
+
+        protected abstract int GetPointSub(int price);
+    }
+}
