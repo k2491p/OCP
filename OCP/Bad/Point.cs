422de1c7388bbbfbabe10373d24f4fc486f5eeeb
fukuda <kfukuda@student.42tokyo.jp>
2022-03-02 18:55:52 +0900

    add Badコードと実行例追加

---
+++ OCP/Bad/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCP.Bad
+{
+    public sealed class Point
+    {
+        public int GetPoint(int price, string memberNo)
+        {
+            if (memberNo.StartsWith("G"))
+            {
+                return (int)(price * 0.03);
+            }
+
+            if (memberNo.StartsWith("S"))
+            {
+                return (int)(price * 0.02);
+            }
+
+            return (int)(price * 0.01);
+
+        }
+    }
+}
--- OCP/Program.cs
+++ OCP/Program.cs
@@ -1,6 +1,7 @@
 ﻿using OCP.Factories;
-using OCP.GoodAbstract;
 using OCP.GoodInterface;
+//using OCP.GoodAbstract;
+//using OCP.GoodInterface;
 using System;
 
 namespace OCP
@@ -9,16 +10,36 @@ namespace OCP
     {
         static void Main(string[] args)
         {
+            IPoint _point;
             var memberNo = args[0].ToString();
             int price = Convert.ToInt32(args[1]);
 
             // GoodInterfaceを動かす例
-            IPoint point = Factory.CreateIPoint(memberNo);
-            Console.WriteLine($"メンバー:{memberNo}は{price}円で{point.GetPoint(price)}ポイント獲得！");
+            //IPoint point = Factory.CreateIPoint(memberNo);
+            //Console.WriteLine($"メンバー:{memberNo}は{price}円で{point.GetPoint(price)}ポイント獲得！");
 
             // GoodAbstractを動かす例
-            PointBase point2 = Factory.CreatePointBase(memberNo);
-            Console.WriteLine($"メンバー:{memberNo}は{price}円で{point2.GetPoint(price)}ポイント獲得！");
+            //PointBase point2 = Factory.CreatePointBase(memberNo);
+            //Console.WriteLine($"メンバー:{memberNo}は{price}円で{point2.GetPoint(price)}ポイント獲得！");
+
+            // Badを動かす例
+            if (memberNo.StartsWith("G"))
+            {
+                _point = new GoldMemberPoint();
+                Console.WriteLine($"メンバー:{memberNo}は{price}円で{_point.GetPoint(price)}ポイント獲得！");
+            }
+            else if (memberNo.StartsWith("S"))
+            {
+                _point = new SilverMemberPoint();
+                Console.WriteLine($"メンバー:{memberNo}は{price}円で{_point.GetPoint(price)}ポイント獲得！");
+            }
+            else
+            {
+                _point = new NormalMemberPoint();
+                Console.WriteLine($"メンバー:{memberNo}は{price}円で{_point.GetPoint(price)}ポイント獲得！");
+
+            }
+
         }
     }
 }
